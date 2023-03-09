8c04d602c3db262efb1fc78b411a5d6d1c21fc32
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-03-08 23:28:10 -0300

    Exercício de tabuada

--- aula14ex/ex018/script.js
+++ aula14ex/ex018/script.js
@@ -1,1 +1,19 @@
-alert('Olá, Mundo!')
+function tabuada() {
+    var txtNum = document.getElementById('num')
+    var tabuada = document.getElementById('tab')
+
+    if (txtNum.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        var n = Number(txtNum.value)
+        var c = 1
+        tabuada.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tabuada${c}`
+            tabuada.appendChild(item)
+            c++
+        }
+    }
+}
